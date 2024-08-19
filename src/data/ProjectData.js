6454c13c69fa2f47ac35210e71d7e937cd4a6f9e
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-08-19 17:49:15 +0530

    project data

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -23,6 +23,17 @@ export const ProjectList = [
     demo: true,
   },
   {
+    img: "/assets/Projects/project4.png",
+    title: "Media Storage Website",
+    description:
+      "This is a robust media uploading and storage website built leveraging the features of Amplify and GraphQL. Built to handle 10,000 traffic at any instant time with photos getting uploaded in S3 buckets.",
+    tech_stack: "Typescript, AWS-Amplify, Dynamo-DB, S3 Buckets, GraphQL, aws-ec2, aws-cli",
+    github_url: "https://github.com/ritankarsaha/Perosnal-Blogging",
+    live_url: "https://master.d1cdu92luafnrq.amplifyapp.com/",
+    demo_youtube:"",
+    demo: true,
+  },
+  {
     img: "/assets/Projects/project3.png",
     title: "Full-Stack Netflix-Clone",
     description:
@@ -46,6 +57,88 @@ export const ProjectList = [
   }
 ];
 
+
+// ------------------------------------------------------------Boiler Plate Projects List
+export const BoilerPlateProjectList = [
+  {
+    img: "/assets/BoilerPlateProjects/project2.png",
+    title: "JWT Auth System in Golang",
+    description:
+      "This is an implementation of full jwt authentication in golang from scratch without the help of external libraries. Completely open-source.",
+    tech_stack: "Golang",
+    github_url: "https://github.com/ritankarsaha/Golang-JWT-Auth",
+    live_url: "https://github.com/ritankarsaha/Golang-JWT-Auth/blob/main/README.md",
+    demo_youtube:"",
+    demo: true,
+  },
+  {
+    img: "/assets/BoilerPlateProjects/Work.jpeg",
+    title: "Golang Backend Boiler-Plate",
+    description:
+      "This is a full stack backend reusable boilerplate code built on Golang ready to be integrated and used in Frotend/Full-Stack Project. Still under development.",
+    tech_stack: "Golang",
+    github_url: "https://github.com/ritankarsaha/Go-Backend-Fullscale",
+    live_url: "",
+    demo_youtube:"",
+    demo: true,
+  },
+  {
+    img: "/assets/BoilerPlateProjects/project1.png",
+    title: "MERN Backend Boiler-Plate",
+    description:
+      "This is a full stack backend reusable boilerplate code built on Express.js and Node.js ready to be integrated and used in Frotend/Full-Stack Project.",
+    tech_stack: "Express.js, Node.js",
+    github_url: "https://github.com/ritankarsaha/Backend-BoilerPlate-Express-Node",
+    live_url: "https://github.com/ritankarsaha/Backend-BoilerPlate-Express-Node/blob/main/README.md",
+    demo_youtube:"https://www.youtube.com/playlist?list=PLhVXjvDUxfbSssTZcX44GH7fLSrgC-1jP",
+    demo: true,
+  },
+  {
+    img: "/assets/BoilerPlateProjects/project4.png",
+    title: "Ecommerce App Robust Backend",
+    description:
+      "This is a robust backend of an Ecommerce Web Application built using Express and Node with strong use of my own built boilerplate code. Ready to be integrated with any frontend.",
+    tech_stack: "Express.js, Node.js",
+    github_url: "https://github.com/ritankarsaha/Ecommerce-Web-App",
+    live_url: "https://github.com/ritankarsaha/Ecommerce-Web-App/blob/main/README.md",
+    demo_youtube:"",
+    demo: true,
+  },
+
+  {
+    img: "/assets/BoilerPlateProjects/Work.jpeg",
+    title: "NEXT.JS Boiler-Plate",
+    description:
+      "This is a full stack backend reusable production-grade boilerplate code built on the robust full-stack framework Next.js ready to be used for any project",
+    tech_stack: "Express.js, Node.js",
+    github_url: "https://github.com/ritankarsaha/Next-JS-Full-Stack-Boilerplate-Code",
+    live_url: "https://github.com/ritankarsaha/Next-JS-Full-Stack-Boilerplate-Code/blob/main/README.md",
+    demo_youtube:"",
+    demo: true,
+  },
+
+
+ 
+];
+
+// ----------------------------------------------------------Utlity Project List
+export const UtlityProjectList = [
+  {
+    img: "/assets/UtilityProjects/project1.png",
+    title: "Terminal Digital Card",
+    description:
+      "This is a Digital Card being displayed in the terminal in a formatted manner to give a basic portfolio idea about oneself. Can be modified for complete personal use as well.",
+    tech_stack: "Javascript, Bash, CLI, Chalk, Shell",
+    github_url: "https://github.com/ritankarsaha/Digital-Card",
+    live_url: "https://github.com/ritankarsaha/Digital-Card/blob/main/README.md",
+    demo_youtube:"",
+    demo: true,
+  },
+
+
+
+
+];
 
 
 // ----------------------------------------------------- Project Video List
@@ -121,16 +214,31 @@ export const GolangVideosList = [
 export const ExperienceList = [
   
   {
-    title: "GSSoC'24 (GirlScript Summer of Code)",
+    title: "Codemod",
+    description: [
+      "Collaborated on a codebase with 5 of my colleagues and 3 senior developers to setup and facilitate unparalled speed of code migrations in a codebase to provide the developer a smooth and enhanced coding experience",
+      "Worked on community-led codemods enabling developers to automate framework upgrades, large refactoring, and boilerplate programming facilitating unparalleled speed and developer experience.",
+            
+    ],
+    role: "Software Development Intern (August 2024 - Present)",
+    experience_certificate1:
+      "",
+    experience_certificate2:
+    "",
+    experience_certificate3:
+    "",
+  },
+  {
+    title: "GSSoC'24 GirlScript Summer of Code",
     description: [
       "As a keen open-source developer and to prepare for future open-source opportunities like GSoC, SOB, Outreachy, etc., I enrolled and secured selection for the GSSoC’24 program.",
       "I demonstrated active participation in the program by contributing to over 23 repositories, resolving approximately 39 issues, and initiating over 20 issues of my own after thorough analysis of the website.",
-      "I have contributed over 12,897 lines of code, with significant contributions including establishing an entire project’s authentication system from scratch using JWT web tokens and Google OAuth client, and migrating another project’s entire database from legacy MongoDB to Supabase, followed by redeploying the site on Vercel. Additionally, I set up necessary GitHub actions to facilitate future community contributions to the website."
+      "I have contributed over 12,897 lines of code, with significant contributions including establishing an entire project’s authentication system from scratch using JWT web tokens and Google OAuth client, and migrating another project’s entire database from legacy MongoDB to Supabase, followed by redeploying the site on Vercel. Additionally, I set up necessary GitHub actions to facilitate future community contributions to the website. Earned myself the Explorer Badge."
             
     ],
     role: "Open Source Intern (March 2024 - May 2024)",
     experience_certificate1:
-      "",
+    "https://drive.google.com/file/d/1ldA3BgwUlOsCS9_5SbRdHPPFsnHYI72c/view?usp=sharing",
     experience_certificate2:
     "https://drive.google.com/file/d/15QFYPRyeasNEGaep9x7mncBi4fFS5-Fs/view?usp=sharing",
     experience_certificate3:
@@ -149,7 +257,7 @@ export const AwardsList = [
     ],
     role: "Open-Source Developer",
     award_certificate1:
-      "",
+      "https://drive.google.com/file/d/1ldA3BgwUlOsCS9_5SbRdHPPFsnHYI72c/view?usp=sharing",
     award_certificate2:
     "https://drive.google.com/file/d/15QFYPRyeasNEGaep9x7mncBi4fFS5-Fs/view?usp=sharing",
     isc: true,
